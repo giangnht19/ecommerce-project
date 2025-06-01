1302892367cc2db4d2f3be52469797d0440552ed
Huu Truong Giang Nguyen <s223197048@deakin.edu.au>
2025-06-01 14:10:42 +1000

    Update ShopContext.jsx

--- client/src/Context/ShopContext.jsx
+++ client/src/Context/ShopContext.jsx
@@ -11,7 +11,7 @@ const getDefaultCart = () => {
 };
 
 const ShopContextProvider = ({ children }) => {
-    const server = "http://localhost:4000";
+    const server = "https://ecommerce-server-cj0f.onrender.com";
     const [all_product, setAllProduct] = useState([]);
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
